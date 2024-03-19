e4bf77ad0950ffcbf965d15acc527c54516b9a03
Saeed <dotnettashkent@gmail.com>
2024-03-19 16:06:08 +0500

    [Feat] - implement ProductEntity and other features

---
+++ Shared/Features/Product/ProductView.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Shared.Features.Product
+{
+    public partial class ProductView
+    {
+        [property : DataMember]
+        [property : JsonPropertyName("id")]
+        public Guid Id { get; set; }
+        [property: DataMember]
+        [property: JsonPropertyName("name")]
+        public string Name { get; set; } = null!;
+
+        [property: DataMember]
+        [property: JsonPropertyName("description")]
+        public string? Description { get; set; }
+
+    }
+}
