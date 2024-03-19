a18bc6848fd196b60e4996f1827ff27e76dba0e9
Saeed <dotnettashkent@gmail.com>
2024-03-19 16:32:54 +0500

    [Feat] - implement AppDbContext files

---
+++ Service/Data/AppDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stl.Fusion.EntityFramework;
+using Stl.Fusion.EntityFramework.Authentication;
+using Stl.Fusion.EntityFramework.Extensions;
+using Stl.Fusion.EntityFramework.Operations;
+using Shared.Features;
+
+namespace Dashboard.Services;
+
+public partial class AppDbContext : DbContextBase
+{
+    // Stl.Fusion.EntityFramework tables
+    public DbSet<DbUser<string>> Users { get; protected set; } = null!;
+    public DbSet<DbUserIdentity<string>> UserIdentities { get; protected set; } = null!;
+    public DbSet<DbSessionInfo<string>> Sessions { get; protected set; } = null!;
+    public DbSet<DbKeyValue> KeyValues { get; protected set; } = null!;
+    public DbSet<DbOperation> Operations { get; protected set; } = null!;
+
+    public AppDbContext(DbContextOptions options) : base(options) { }
+
+    public override int SaveChanges()
+    {
+        AddTimestamps();
+        return base.SaveChanges();
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        AddTimestamps();
+        return await base.SaveChangesAsync();
+    }
+
+    private void AddTimestamps()
+    {
+        var entities = ChangeTracker.Entries()
+            .Where(x => x.Entity is BaseEntity && (x.State == EntityState.Added || x.State == EntityState.Modified));
+
+        foreach (var entity in entities)
+        {
+            var now = DateTime.UtcNow; // current datetime
+
+            if (entity.State == EntityState.Added)
+            {
+                ((BaseEntity)entity.Entity).CreatedAt = now;
+            }
+            ((BaseEntity)entity.Entity).UpdatedAt = now;
+        }
+    }
+}
---
+++ Service/Data/CLOUPARDContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stl.Fusion.EntityFramework;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Features;
+
+namespace Dashboard.Services;
+public partial class AppDbContext : DbContextBase
+{
+    public IServiceScopeFactory _serviceScopeFactory;
+    public AppDbContext()
+    {
+
+    }
+
+    [ActivatorUtilitiesConstructor]
+    public AppDbContext(DbContextOptions<AppDbContext> options, IServiceScopeFactory serviceScopeFactory) : base(options)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    public virtual DbSet<ProductEntity> Products { get; set; }
+}
